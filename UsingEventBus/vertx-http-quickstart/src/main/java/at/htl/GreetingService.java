1be7ad785bcbbe8134a6641687ba4850f7ed08bd
PhilipTrinkl <philip.trinkl@gmail.com>
2021-11-26 13:24:37 +0100

    TP, 26.11.2021, created GreetingService.java

---
+++ UsingEventBus/vertx-http-quickstart/src/main/java/at/htl/GreetingService.java
@@ -0,0 +1,15 @@
+package at.htl;
+
+import io.quarkus.vertx.ConsumeEvent;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    @ConsumeEvent("greeting")
+    public String greeting(String name) {
+        return "Hello " + name;
+    }
+
+}
