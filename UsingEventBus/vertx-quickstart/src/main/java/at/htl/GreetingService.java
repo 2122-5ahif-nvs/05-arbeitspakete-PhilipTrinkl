e4b197865c31c5261aa2a3b2a70f4c03655517e1
PhilipTrinkl <philip.trinkl@gmail.com>
2021-11-26 13:01:53 +0100

    TP, 26.11.2021, added second event with return type Uni

--- UsingEventBus/vertx-quickstart/src/main/java/at/htl/GreetingService.java
+++ UsingEventBus/vertx-quickstart/src/main/java/at/htl/GreetingService.java
@@ -1,8 +1,11 @@
 package at.htl;
 
 import io.quarkus.vertx.ConsumeEvent;
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.context.ManagedExecutor;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,8 +14,16 @@ import javax.ws.rs.core.MediaType;
 @ApplicationScoped
 public class GreetingService {
 
-    @ConsumeEvent
+    @Inject
+    ManagedExecutor executor;
+
+    @ConsumeEvent("greeting")
     public String consume(String name) {
         return name.toUpperCase();
+    }
+
+    @ConsumeEvent("greeting")
+    public Uni<String> consume2(String name) {
+        return Uni.createFrom().item(() -> name.toUpperCase()).emitOn(executor);
     }
 }
